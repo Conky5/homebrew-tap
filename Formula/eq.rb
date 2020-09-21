292315e267d6c1d6f2ada1c955e65eed46326259
Chris Koehnke <chris@koehnke.xyz>
2020-09-21 16:41:35 -0400

    Fix eq shasum for linux

--- Formula/eq.rb
+++ Formula/eq.rb
@@ -8,7 +8,7 @@ class Eq < Formula
     sha256 "ced3c0694c68c53014989cf60ac9e46a7aaea819ec03bb990e9105f3954ee0b3"
   elsif OS.linux?
     url "https://github.com/Conky5/eq/releases/download/v0.4.0/eq-v0.4.0-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "e1f8c9c351e714a7a889aa2d4388bb1e37f39d1a1c13d85c921678eb0a1fbc79"
+    sha256 "1e2452396e5bf6e63564d4ac0d384e593dbfd16abd9f5a30fa6ac07ed3428bb6"
   end
 
   def install
