9c0b0e96dd2d9f85c4ca39dfd3cc096c4434fa81
Chris Koehnke <chris@koehnke.xyz>
2021-01-08 13:53:43 -0500

    eq v1.0.1
    Update to eq v1.0.1. Add a license entry. The description convention
    seems to not end with a period so remove it. Add installation of the eq
    man page.

--- Formula/eq.rb
+++ Formula/eq.rb
@@ -1,18 +1,20 @@
 class Eq < Formula
-  desc "A simple command line interface for Elasticsearch queries."
+  desc "A simple command line interface for Elasticsearch queries"
   homepage "https://github.com/Conky5/eq"
-  version "1.0.0"
+  license "Apache-2.0"
+  version "1.0.1"
 
   if OS.mac?
-    url "https://github.com/Conky5/eq/releases/download/v1.0.0/eq-v1.0.0-x86_64-apple-darwin.tar.gz"
-    sha256 "2d1fa6a2def51b3754906d4d6a4ea4f478eb6d65cfa79c38483b09acb4ca7738"
+    url "https://github.com/Conky5/eq/releases/download/v#{version}/eq-v#{version}-x86_64-apple-darwin.tar.gz"
+    sha256 "084f9bb30131f172a5de49ef757a1ecf21e69c2141b00e1b348b226d21109952"
   elsif OS.linux?
-    url "https://github.com/Conky5/eq/releases/download/v1.0.0/eq-v1.0.0-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "1662fce600d04060eab98caee17c0a7aebe47d6afb4d790e87e0a84231128058"
+    url "https://github.com/Conky5/eq/releases/download/v#{version}/eq-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "6810a770c424fee35cad5001f4da2a0c689ae8fdcb824f61cb9c8bbb06212cd8"
   end
 
   def install
     bin.install "eq"
+    man1.install "eq.1"
   end
 
   test do
