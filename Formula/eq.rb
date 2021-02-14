d16f00356078638d0df1ab7e1c8cfd76c205623b
Chris Koehnke <chris@koehnke.xyz>
2021-02-14 18:18:49 -0500

    eq v1.0.2
    Update to eq v1.0.2 which includes an aarch64 binary for linux.

--- Formula/eq.rb
+++ Formula/eq.rb
@@ -2,14 +2,19 @@ class Eq < Formula
   desc "A simple command line interface for Elasticsearch queries"
   homepage "https://github.com/Conky5/eq"
   license "Apache-2.0"
-  version "1.0.1"
+  version "1.0.2"
 
   if OS.mac?
     url "https://github.com/Conky5/eq/releases/download/v#{version}/eq-v#{version}-x86_64-apple-darwin.tar.gz"
-    sha256 "084f9bb30131f172a5de49ef757a1ecf21e69c2141b00e1b348b226d21109952"
+    sha256 "267679723596cd5c0dbe3fecc178673109be070de079637a991320dc3d9f2cf1"
   elsif OS.linux?
-    url "https://github.com/Conky5/eq/releases/download/v#{version}/eq-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "6810a770c424fee35cad5001f4da2a0c689ae8fdcb824f61cb9c8bbb06212cd8"
+    if Hardware::CPU.arm?
+      url "https://github.com/Conky5/eq/releases/download/v#{version}/eq-v#{version}-aarch64-aarch64-linux-gnu.tar.gz"
+      sha256 "65271b580384fd5066cb7bd7a6402c25a3745709b2d252fa66f75d8a7ac3b996"
+    else
+      url "https://github.com/Conky5/eq/releases/download/v#{version}/eq-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "2f853110194a5af91a0da3cd1c3b5e86177378fada26c432a8f7a40f7d5e4806"
+    end
   end
 
   def install
