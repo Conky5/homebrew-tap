1592b9bff46e8807787b38d662d201292d2dd2e9
Chris Koehnke <chris@koehnke.xyz>
2021-02-19 13:50:55 -0500

    eq v1.0.3

--- Formula/eq.rb
+++ Formula/eq.rb
@@ -2,18 +2,18 @@ class Eq < Formula
   desc "A simple command line interface for Elasticsearch queries"
   homepage "https://github.com/Conky5/eq"
   license "Apache-2.0"
-  version "1.0.2"
+  version "1.0.3"
 
   if OS.mac?
     url "https://github.com/Conky5/eq/releases/download/v#{version}/eq-v#{version}-x86_64-apple-darwin.tar.gz"
-    sha256 "267679723596cd5c0dbe3fecc178673109be070de079637a991320dc3d9f2cf1"
+    sha256 "8f43e5c2133dbcc3f3befce83af5742159bd4aea2ca4dbff120d75584ed98e81"
   elsif OS.linux?
     if Hardware::CPU.arm?
       url "https://github.com/Conky5/eq/releases/download/v#{version}/eq-v#{version}-aarch64-aarch64-linux-gnu.tar.gz"
-      sha256 "65271b580384fd5066cb7bd7a6402c25a3745709b2d252fa66f75d8a7ac3b996"
+      sha256 "99e45ac645cd996d2885132bfaaa623f01e42ec861aa8e6775287cb233506885"
     else
       url "https://github.com/Conky5/eq/releases/download/v#{version}/eq-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "2f853110194a5af91a0da3cd1c3b5e86177378fada26c432a8f7a40f7d5e4806"
+      sha256 "1ed240a79e8fec533b86e7c79b3e379d7a3cc226763bb515140165b97a8e1b8c"
     end
   end
 
